6f4517a9f6331d7a30b30adaa108821cfb8b7bb0
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-17 12:29:47 -0500

    Creando maquetacion del proyecto

---
+++ resources/views/app.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+@include('layouts.head')
+<body>
+	{{-- HEADER --}}
+@include('layouts.header')
+	{{-- SECTION --}}
+@include('layouts.section')
+	{{-- ASIDE --}}
+@include('layouts.aside')
+	{{-- FOOTER --}}
+@include('layouts.footer')
+</body>
+</html>
---
+++ resources/views/layouts/head.blade.php
@@ -0,0 +1,28 @@
+<head>
+	<title>BeProArt</title>
+	<style type="text/css">
+		header{
+			border: 2px solid black;
+			width: 100%;
+			height: 50px;
+		}
+		section{
+			border: 2px solid black;
+			float:  left;
+			margin-left: 10%;
+			width: 90%;
+			height: 900px;
+		}
+		aside{
+			border: 2px solid black;
+			/* float:  left; */
+			width: 10%;
+			height: 900px;
+		}
+		footer{
+			border: 2px solid black;
+			width: 100%;
+			height: 50px;
+		}
+	</style>
+</head>
