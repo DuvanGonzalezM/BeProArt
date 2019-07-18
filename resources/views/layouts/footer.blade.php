ff747e6ed183602128a49070ebfe5fa296a64aef
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-18 10:27:00 -0500

    Añadiendo footer

--- resources/views/layouts/footer.blade.php
+++ resources/views/layouts/footer.blade.php
@@ -1,3 +1,13 @@
-<footer>
-	
+<footer class="navbar navbar-expand">
+	<div id="contactUs" class="col-lg-2">
+		<a href="#"><i class="fas fa-envelope"></i> Contactenos</a>
+	</div>
+	<div id="aboutUs" class="col-lg-2">
+		<a href="#"><i class="fas fa-address-card"></i> Sobre Nosotros</a>
+	</div>
+	<div id="affiliates" class="col-lg-8">
+		<div class="col-lg-12">
+			<label><b>Empresas Asociadas</b></label>
+		</div>
+	</div>
 </footer>
