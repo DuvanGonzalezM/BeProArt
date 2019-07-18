7ee59f150a9b0b956d4cd214e6dbe0557d181907
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-18 17:15:13 -0500

    Agreagando primera tabla a la base de datos

---
+++ database/migrations/2019_07_18_221104_productos_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class ProductosTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('productos', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('link')->unique();
+            $table->string('precio', 64)->nullable();
+            $table->string('categoria')->nullable();
+            $table->string('pagina')->nullable();
+            $table->string('descripcion')->nullable();
+            $table->engine = 'InnoDB';
+            $table->charset = 'utf8';
+            $table->collation = 'utf8_unicode_ci';
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('productos');
+    }
+}
