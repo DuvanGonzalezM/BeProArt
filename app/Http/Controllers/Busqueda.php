81fedebb19e2c3398b46e9b9867d0730ba994f7c
LoquilloDuvan <programmerloquillo@gmail.com>
2019-07-24 15:28:40 -0500

    Se cierra proyecto ya que no es inobador y hay varios sitos con la misma vision

--- app/Http/Controllers/Busqueda.php
+++ app/Http/Controllers/Busqueda.php
@@ -22,29 +22,57 @@ class Busqueda extends Controller
 		$UrlAmazon = 'https://www.amazon.com/s?k='.$busqueda.'&s=price-asc-rank&__mk_es_US=%C3%85M%C3%85%C5%BD%C3%95%C3%91&qid=1563986782&ref=sr_st_price-asc-rank';
 		$BusquedaAma = file_get_contents($UrlAmazon, false, $contexto);
 		$AmaHtml = new Crawler($BusquedaAma);
-		$items = $AmaHtml->filter('div.sg-col-4-of-24 > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)');
-
+		$items = $AmaHtml->filter('div.sg-col-4-of-24 > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2)');
+		$items2 = $AmaHtml->filter('div.s-result-item > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1)');
 		$divs = [];
-		for($i = 0; $i < count($items); $i++){
-			$imgs = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1) > div:nth-child(1) > img:nth-child(1)')->extract(['src']);
-			$price1 = $AmaHtml->filter('div.sg-col-4-of-24:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span.a-offscreen')->extract('_text');
-			$price2 = $AmaHtml->filter('div.sg-col-4-of-24:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span.a-offscreen')->extract('_text');
-			$text = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1) > span:nth-child(1)')->extract('_text');
-			$hrefs = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1)')->extract(['href']);
-			$divs[$i]['img'] = $imgs[0];
-			$divs[$i]['descript'] = $text[0];
-			$divs[$i]['link'] = 'https://www.amazon.com'.$hrefs[0];
-			if(isset($price1[0])){
-				$divs[$i]['price'] = $price1[0];
-			}
-			elseif(isset($price2[0])){
-				$divs[$i]['price'] = $price2[0];
-			}
-			else{
-				$divs[$i]['price'] = "No se dio precio";
+		return count($items2);
+		if(count($items) > 0){
+			for($i = 0; $i < count($items); $i++){
+				$imgs = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1) > div:nth-child(1) > img:nth-child(1)')->extract(['src']);
+				$price1 = $AmaHtml->filter('div.sg-col-4-of-24:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span.a-offscreen')->extract('_text');
+				$price2 = $AmaHtml->filter('div.sg-col-4-of-24:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span.a-offscreen')->extract('_text');
+				$text = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1) > span:nth-child(1)')->extract('_text');
+				$hrefs = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1)')->extract(['href']);
+				$divs[$i]['img'] = $imgs[0];
+				$divs[$i]['descript'] = $text[0];
+				$divs[$i]['link'] = 'https://www.amazon.com'.$hrefs[0];
+				if(isset($price1[0])){
+					$divs[$i]['price'] = $price1[0];
+				}
+				elseif(isset($price2[0])){
+					$divs[$i]['price'] = $price2[0];
+				}
+				else{
+					$divs[$i]['price'] = "No se dio precio";
+				}
 			}
 		}
+		elseif(count($items2) > 0){
+			for($i = 0; $i < count($items2); $i++){
+				$imgs = $AmaHtml->filter('div.sg-col-20-of-24:nth-child(7) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1) > div:nth-child(1) > img:nth-child(1)')->extract(['src']);
+				$price1 = $AmaHtml->filter('div.sg-col-20-of-24:nth-child(7) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span:nth-child(1)')->extract('_text');
+				$price2 = $AmaHtml->filter('div.sg-col-4-of-24:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(3) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > a:nth-child(1) > span:nth-child(1) > span.a-offscreen')->extract('_text');
+				$text = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > h2:nth-child(1) > a:nth-child(1) > span:nth-child(1)')->extract('_text');
+				$hrefs = $AmaHtml->filter('div.s-result-item:nth-child('.($i+1).') > div > div:nth-child(1) > div:nth-child(1) > div:nth-child(2) > div:nth-child(1) > div:nth-child(1) > div:nth-child(1) > span:nth-child(1) > a:nth-child(1)')->extract(['href']);
+				$divs[$i]['img'] = $imgs[0];
+				$divs[$i]['descript'] = $text[0];
+				$divs[$i]['link'] = 'https://www.amazon.com'.$hrefs[0];
+				if(isset($price1[0])){
+					$divs[$i]['price'] = $price1[0];
+				}
+				elseif(isset($price2[0])){
+					$divs[$i]['price'] = $price2[0];
+				}
+				else{
+					$divs[$i]['price'] = "No se dio precio";
+				}
+			}
+		}
+		else{
+			abort(404);
+		}
 		$AmaHtml->clear();
-		return view('busqueda', compact('divs'));
+		// return $divs;
+		return view('busqueda', compact('divs', 'UrlAmazon'));
 	}
 }
--- resources/views/busqueda.blade.php
+++ resources/views/busqueda.blade.php
@@ -2,6 +2,30 @@
 @section('title', 'Home')
 @section('section')
 	<section>
-		<H1>{{$_GET['seeker']}}</H1>
+		<h1>{{$UrlAmazon}}</h1>
+		<div class="col-lg-16">
+			<div class="title">
+				<label class="h3">Resultados de: <b>{{$_GET['seeker']}}</b></label>
+			</div>
+			<div class="container">
+				<div class="row">
+					@foreach($divs as $div)
+					<div class="card resul-item">
+						<a href="{{$div['link']}}">
+							<img src="{{$div['img']}}" class="card-img-top" alt="{{$div['descript']}}" style="width: 100%; height: 10rem;">
+							<div class="card-body">
+								<h5 class="card-title">
+									<b>{{$div['price']}}</b>
+								</h5>
+								<p class="card-text">
+									{{$div['descript']}}
+								</p>
+							</div>
+						</a>
+					</div>
+					@endforeach
+				</div>
+			</div>
+		</div>
 	</section>
 @endsection
